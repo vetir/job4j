98b31aafd97628939f7347c7c9409540a0b23048
vetir@mail.ru <vetir@mail.ru>
2022-01-11 21:04:56 +0300

    For Merge

---
+++ src/main/java/git/exel/FirstExel.java
@@ -0,0 +1,10 @@
+package git.exel;
+
+public class FirstExel {
+    public static void main(String[] args) {
+        HSSFCell cell = row.createCell(0);
+        double d = cell.getNumericCellValue();
+        cell.setCellValue(d);
+    }
+    HSSFCell cell = row.createCell(0);
+}
